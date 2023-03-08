98ef54b2626ce8dcdf76f594f7911444cac341a0
HoangVAnhThy <anhthysss12ht@gmail.com>
2023-03-08 11:08:13 +0700

    ThyHVA - commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,29 @@
+import { useState } from "react";
+
 const Navbar = () => {
+
+    const [authFormData, setAuthFormData] = useState({userName: '' , password: ''});
+    
+    const handlerSubmit = () => {
+        let options = {
+            method: 'POST',
+            headers:{
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(authFormData)
+        };
+        let signInAPI = "http://localhost:9000/authorize/signIn";
+        fetch(signInAPI, options)
+        .then(res => {
+            if(res.status === 200){
+                alert("Đăng nhập thành công");
+            }
+        })
+        .catch(() => {
+            alert("Đăng nhập thất bại");
+        })
+    }
+
     return (
         <section>
             <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
@@ -68,11 +93,29 @@ const Navbar = () => {
                                 <form class="space-y-6" action="#">
                                     <div>
                                         <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-                                        <input type="username" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Username" required />
+                                        <input 
+                                        type="username" 
+                                        name="userName" 
+                                        id="userName" 
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" 
+                                        placeholder="Username" 
+                                        required 
+                                        value = {authFormData.userName}
+                                        onChange = {e => setAuthFormData({...authFormData, userName: e.target.value})}
+                                        />
                                     </div>
                                     <div>
                                         <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                                        <input 
+                                        type="password" 
+                                        name="password" 
+                                        id="password" 
+                                        placeholder="••••••••" 
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" 
+                                        required
+                                        value = {authFormData.password}
+                                        onChange = {e => setAuthFormData({...authFormData, password: e.target.value})}
+                                        />
                                     </div>
                                     <div class="flex justify-between">
                                         <div class="flex items-start">
@@ -83,7 +126,7 @@ const Navbar = () => {
                                         </div>
                                         <a href="#" class="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
                                     </div>
-                                    <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
+                                    <button type="submit" onClick={handlerSubmit} class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
                                     <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
                                         Not registered? <a href="#" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
                                     </div>
